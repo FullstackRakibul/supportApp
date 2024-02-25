4d63e508457c8b6b44f6b93a396bafdd4fc5a439
rakibul <rakibul.cse.47@gmail.com>
2024-02-25 17:11:52 +0600

    AgentDashboardAdded

--- SupportApp/ClientApp/src/utils/userDetails.jsx
+++ SupportApp/ClientApp/src/utils/userDetails.jsx
@@ -1,18 +1,6 @@
-// const useDetails = () => {
-//   const userData = localStorage.getItem("user");
-//   console.log(userData);
-//   const { email, name, empCode, phoneNumber } = JSON.parse(userData);
-
-//   return { email, name, empCode, phoneNumber };
-// };
-
-// export default useDetails;
-
 const useDetails = () => {
   const userData = localStorage.getItem("user");
   console.log(userData);
-  const { email, name, empCode, phoneNumber } = JSON.parse(userData);
-
   let email = "";
   let name = "";
   let empCode = "";
