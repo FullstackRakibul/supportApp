3b74223931c0fc113a2d6ed093628549b66a8f07
FullStackRakibul <rakibul.cse.47@gmail.com>
2024-02-26 22:28:09 +0600

    safeCommit26Feb

--- SupportApp/ClientApp/src/utils/userDetails.jsx
+++ SupportApp/ClientApp/src/utils/userDetails.jsx
@@ -1,10 +1,6 @@
 const useDetails = () => {
   const userData = localStorage.getItem("user");
   console.log(userData);
-<<<<<<< HEAD
-
-=======
->>>>>>> b8822d5cdc2d84fc3203793d3b3dda524d36a7e2
   let email = "";
   let name = "";
   let empCode = "";
