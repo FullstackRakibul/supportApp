24e0b92af1cef82791f3ee1b81fb8ca6ef38a028
FullStackRakibul <rakibul.cse.47@gmail.com>
2024-03-09 00:53:00 +0600

    pagination query api create

---
+++ SupportApp/Service/Pagination/PaginationService.cs
@@ -0,0 +1,23 @@
+﻿using SupportApp.Models;
+
+namespace SupportApp.Service.Pagination
+{
+    public class PaginationService : IPaginationService
+    {
+        private readonly SupportAppDbContext _context;
+
+        public PaginationService(SupportAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Ticket> GetPaginationList(int currentPage, int pageSize)
+        {
+            int skip = (currentPage - 1) * pageSize;
+            return _context.Ticket.OrderByDescending(t => t.CreatedAt)
+                                   .Skip(skip)
+                                   .Take(pageSize)
+                                   .ToList();
+        }
+    }
+}
