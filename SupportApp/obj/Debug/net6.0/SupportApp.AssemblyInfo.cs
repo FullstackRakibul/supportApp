62c2efa20d03702863aabf748c4b186ba2f2016b
FullstackRakibul <rakibul.cse.47@gmail.com>
2024-04-29 08:41:01 +0600

    safeCommitOn29Aprl24

--- SupportApp/obj/Debug/net6.0/SupportApp.AssemblyInfo.cs
+++ SupportApp/obj/Debug/net6.0/SupportApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupportApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad67ba6779be5f95d226365ec4bd8ce532a93787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8366d26c79aedd2d3165a5fb72c250056ede9d93")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupportApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupportApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
