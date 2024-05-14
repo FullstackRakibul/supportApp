b52e2a520bfea0caa04549173b72d68602c2e8c3
FullstackRakibul <rakibul.cse.47@gmail.com>
2024-05-14 16:43:37 +0600

    oldCODE

--- SupportApp/obj/Debug/net6.0/SupportApp.AssemblyInfo.cs
+++ SupportApp/obj/Debug/net6.0/SupportApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupportApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101519ae2cb5f5cb50f5f5e5b16c554feefe30e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea9dd3303e0d9c1d0c4401c557cba1214e2b1717")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupportApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupportApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
